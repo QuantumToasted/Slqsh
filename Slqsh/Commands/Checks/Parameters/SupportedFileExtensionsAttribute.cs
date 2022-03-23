994dea29d286a005ad6a52219b201c5585db531a
Daniel Jarski <danieljarski@gmail.com>
2022-03-23 12:44:15 -0500

    0.4.2
    - Added SupportedFileExtensionsAttribute for checking an attachment's file extension against a pre-defined list

---
+++ Slqsh/Commands/Checks/Parameters/SupportedFileExtensionsAttribute.cs
@@ -0,0 +1,31 @@
+﻿using Disqord;
+using Qmmands;
+
+namespace Slqsh;
+
+public sealed class SupportedFileExtensionsAttribute : SlashCommandParameterCheckAttribute
+{
+    public SupportedFileExtensionsAttribute(params string[] extensions)
+    {
+        if (extensions.Length == 0)
+            throw new ArgumentException("Supported extensions array must not be empty.", nameof(extensions));
+
+        Extensions = extensions;
+    }
+
+    public string[] Extensions { get; }
+
+    public override ValueTask<CheckResult> CheckAsync(object argument, SlashCommandContext context)
+    {
+        var attachment = (IAttachment) argument;
+        var split = attachment.FileName.Split('.');
+        if (split.Length < 2)
+            return Failure("The supplied attachment must have a file extension.");
+
+        var extension = split[^1];
+        return Extensions.Contains(extension, StringComparer.InvariantCultureIgnoreCase)
+            ? Success()
+            : Failure("The supplied attachment must be one of the following types: " +
+                      string.Join(", ", Extensions.Select(x => Markdown.Code(x))));
+    }
+}
